1416ed86f17f7404acf7b3df6f4fed9012fa95e0
Bowie Sessions <eviltoon@yahoo.com>
2019-07-23 12:53:29 -0500

    Updated roster

--- Asker/ask.js
+++ Asker/ask.js
@@ -2,7 +2,7 @@
 	// Isolate the targeted div, define the class, and have fun with variables.
 const answerBox = document.getElementById("answerer");
 // require('classList.js');
-let classList = ["Alexandria Wagner","Anthony Dominguez","Benjamin Escorcia","Brandon Stark","Brian Samuels","Calah Hansen","Cameron Lattz","Christian Breyer","Christopher Krinkie","Colin Kramer","Daniel Merkl","Daniel Thao","Elianice Gorniak","Isaac Johnson","Jack Meuwissen","Jackson Oppenheim","Jason Ostergren","Jon Alden","Kong Yang","Nermin Ceric","Nicholas Wilson","Nolan Hewitt","Sean D'Amico","Seifu Dagnachew","Shane Nelson","Shawn Wallin","Syed Akbar","Tori Johnston","Zeph Tambornino","Zaur Tsagolov"];
+let classList = ["Alexandria Wagner","Anthony Dominguez","Ben Escorcia","Brandon Stark","Brian Samuels","Calah Hansen","Cameron Lattz","Christian Breyer","Christopher Krinkie","Colin Kramer","Daniel Merkl","Daniel Thao","Elia Gorniak","Isaac Johnson","Jack Meuwissen","Jackson Oppenheim","Jason Ostergren","Jon Alden","Kong Yang","Nermin Ceric","Nicholas Wilson","Nolan Hewitt","Sean D'Amico","Seifu Dagnachew","Shane Nelson","Shawn Wallin","Syed Akbar","Tori Johnston","Zeph Tambornino","Zaur Tsagolov"];
 let answerer = classList.slice();
 let alreadyAsked = [];
 
