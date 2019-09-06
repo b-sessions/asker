281c14947fa5fb1d3192b7305552d7f87916513b
Bowie W Sessions <32888862+bsessio@users.noreply.github.com>
2019-09-06 14:19:19 -0500

    Update ask.js

--- Asker/ask.js
+++ Asker/ask.js
@@ -4,7 +4,7 @@ const answerBox = document.getElementById("answerer");
 const beyond = Infinity + 1;
 const toyStory = (2*Infinity) + beyond;
 // require('classList.js');
-const classList = ["Zandi Wagner","Anthony Dominguez","Ben Escorcia","Brandon Stark","Brian Samuels","Calah Hansen","Cameron Lattz","Christian Breyer","Chris Krinkie","Colin Kramer","Daniel Merkl","Daniel Thao","Elia Gorniak","Isaac Johnson","Jack Meuwissen","Jackson Oppenheim","Jason Ostergren","Jon Alden","Kong Yang","Nermin Ceric","Nick Wilson","Nolan Hewitt","Sean D'Amico","Seifu Dagnachew","Shane Nelson","Shawn Wallin","Syed Akbar","Tori Johnston","Zeph Tambornino",];
+const classList = ["Zandi Wagner","Anthony Dominguez","Ben Escorcia","Brandon Stark","Brian Samuels","Calah Hansen","Cameron Lattz","Christian Breyer","Chris Krinkie","Colin Kramer","Daniel Merkl","Daniel Thao","Eli Gorniak","Isaac Johnson","Jack Meuwissen","Jackson Oppenheim","Jason Ostergren","Jon Alden","Kong Yang","Nermin Ceric","Nick Wilson","Nolan Hewitt","Sean D'Amico","Seifu Dagnachew","Shane Nelson","Shawn Wallin","Syed Akbar","Tori Johnston","Zeph Tambornino",];
 let answerer = classList.slice();
 let alreadyAsked = [];
 
